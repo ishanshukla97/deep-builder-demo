7b65f9159a7f2f67aaedd4c41b453f8aa24422e7
ishan <ishanshukla97@gmail.com>
2020-05-16 02:01:12 +0530

    test: more integration tests for modelBuilder page

--- cypress/integration/ModelBuilder.int.spec.ts
+++ cypress/integration/ModelBuilder.int.spec.ts
@@ -37,18 +37,38 @@ describe('model builder page', () => {
     });
 })
 
-describe('can connect two op nodes', () => {
+const addTwoNode = (name1: string, name2: string) => {
+    cy.findByText(name1).drag('[data-testid=playground-widget-main]', {position: 'left'})
+    cy.findByText(name2).drag('[data-testid=playground-widget-main]')
+}
+
+describe('build simple model and download', () => {
     it('can add 2 op node', () => {
         cy.viewport(1024, 720).visit('/')
-        cy.findByText('input').drag('[data-testid=playground-widget-main]', {position: 'left'})
-        cy.findByText('conv2d').drag('[data-testid=playground-widget-main]')
+        addTwoNode('input', 'conv2d')
     })
+    
     it('can link them together', () => {
-        cy.get('[data-name=out]').eq(0).drag('[data-name=in]', {nth: 1});
+        cy.get('[data-name=out]').eq(0).drag('[data-name=in]', {nth: 1}).wait(2000);
+    })
+
+    it('displays error when invalid model is downloaded', () => {
+        cy.findByTestId('btn-download').click()
+        cy.get('.Toastify__toast-body')
+    })
+
+    it('can register layer configuration', () => {
+        cy.findAllByText('input').eq(1).click();
+        cy.findByText('shape').siblings('input').type('150,150,3');
+        cy.findAllByText('conv2d').eq(1).click();
+        cy.findByText('filters').siblings('input').type('10')
+        cy.findByText('kernelSize').siblings('input').type('2,2')
+        cy.findByTestId('playground-widget-main').click()
+    })
+
+    it('populates link labels on click download', () => {
+        cy.findByTestId('btn-download').click().wait(10)
+        cy.findByTestId('playground-widget-main').click()
+        cy.findByText('???,150,150,3')
     })
 })
-
-//@todo
-describe('can download a simple model')
-describe('displays error incorrect model is downloaded')
-describe('populates link labels with shapes')
